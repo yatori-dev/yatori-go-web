59045509cf46c3e3dfce9bc03b88ed1b77d2c0e5
Changbaiqi <changbaiqi@foxmail.com>
2025-06-07 17:05:15 +0800

    update: first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import 'ant-design-vue/dist/reset.css'
+const app = createApp(App)
+// app.use(Ant)
+app.mount('#app')
